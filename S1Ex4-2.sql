953db0eb015c5864cc505fff355a8ad28930f63e
MPilar23 <136824354+MPilar23@users.noreply.github.com>
2023-10-24 19:18:41 +0200

    Add files via upload

---
+++ S1Ex4-2.sql
@@ -0,0 +1,7 @@
+
+SELECT tb_person.person_name, tb_movie_person.person_id, tb_movie_person.movie_id, COUNT(tb_movie_person.role_id)
+FROM tb_person
+INNER JOIN tb_movie_person
+ON tb_person.person_id=tb_movie_person.person_id
+GROUP BY tb_person.person_name, tb_movie_person.person_id, tb_movie_person.movie_id
+HAVING COUNT(tb_movie_person.role_id) > 1;
